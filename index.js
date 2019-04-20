83b7711c3cd8cd1f9a43f105019fb3fbd1c067a3
Silvman <anoscillator@gmail.com>
2019-04-21 00:26:35 +0300

    add debugging environment variable

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ var fetch = require('node-fetch');
 var bodyParser = require('body-parser');
 var fs = require("fs");
 
+let isDebug = false;
+if (process.env.DEBUG) {
+    isDebug = true;
+}
+
 let config = JSON.parse(fs.readFileSync('conf.json', 'utf8'));
 
 app.use(bodyParser.json());
@@ -14,9 +19,12 @@ let x = 0;
 let rtcUsers = {};
 
 io.on('connection', async function(socket){
-    console.log('a user connected', socket.handshake.query.token);
-    console.log(socket.handshake.query);
-    console.log(config.url+"/server/getuserbykey?token="+config.token+"&key="+socket.handshake.query.token);
+    if (isDebug) {
+        console.log('a user connected', socket.handshake.query.token);
+        console.log(socket.handshake.query);
+        console.log(config.url+"/server/getuserbykey?token="+config.token+"&key="+socket.handshake.query.token);
+    }
+
     let data = await (await fetch(config.url+"/server/getuserbykey?token="+config.token+"&key="+socket.handshake.query.token)).json();
     let iUserId = null;
     let sUserAvatar = data.sUserAvatar;
@@ -24,7 +32,10 @@ io.on('connection', async function(socket){
     if (data.iUserId) {
         iUserId = data.iUserId;
         socket.join("user_" + iUserId);
-        console.log('a user connected id ', iUserId);
+
+        if (isDebug) {
+            console.log('a user connected id ', iUserId);
+        }
     }
     socket.on('listenTopic', async function(data){
         let r = await fetch(config.url+"/server/hastopicaccess?token="+config.token+"&userId="+iUserId+"&topicId="+data.id);
@@ -32,7 +43,10 @@ io.on('connection', async function(socket){
         let d = await r.json();
         // console.log(d, iUserId)
         if (!d.bAccess) {
-            console.log("FALSE!");
+            if (isDebug) {
+                console.log("FALSE!");
+            }
+
             return false
         }
         socket.join("topic_"+data.id)
@@ -43,73 +57,126 @@ io.on('connection', async function(socket){
         let d = await r.json();
         // console.log(d, iUserId)
         if (!d.bAccess) {
-            console.log("FALSE!");
+            if (isDebug) {
+                console.log("FALSE!");
+            }
+
             return false
         }
         socket.join("talk_"+data.id)
     });
     socket.on('listenOpenBlogs', function(data){
         socket.join("openBlogListeners");
-        console.log("LISTEN OPEN BLOGS");
+        if (isDebug) {
+            console.log("LISTEN OPEN BLOGS");
+        }
     });
     socket.on('joinRTC',function(){
         socket.join('webrtc');
-        console.log(rtcUsers,user);
+
+        if (isDebug) {
+            console.log(rtcUsers,user);
+        }
+
         rtcUsers[user.id] = user;
-        console.log(rtcUsers,user);
+
+        if (isDebug) {
+            console.log(rtcUsers,user);
+        }
+
         io.sockets.emit('user joined', rtcUsers)
     });
     socket.on('leaveRTC',function(){
         socket.leave('webrtc');
-        console.log(rtcUsers,user);
+
+        if (isDebug) {
+            console.log(rtcUsers,user);
+        }
+
         delete rtcUsers[user.id];
-        console.log(rtcUsers,user);
+
+        if (isDebug) {
+            console.log(rtcUsers,user);
+        }
+
         io.sockets.emit('user leaved', rtcUsers)
     });
     socket.on('getRTC',function(){
-        console.log(rtcUsers,user);
+        if (isDebug) {
+            console.log(rtcUsers,user);
+        }
+
         socket.emit('rtc users', rtcUsers)
     });
     socket.on('speaking', function(){
-        console.log('speaking', iUserId);
+        if (isDebug) {
+            console.log('speaking', iUserId);
+        }
+
         io.sockets.emit('speaking', iUserId)
     });
     socket.on('stopped speaking', function(){
-        console.log('stopped speaking', iUserId);
+        if (isDebug) {
+            console.log('stopped speaking', iUserId);
+        }
+
         io.sockets.emit('stopped speaking', iUserId)
     });
     socket.on('mute', function(){
-        console.log('mute', iUserId);
+        if (isDebug) {
+            console.log('mute', iUserId);
+        }
+
         rtcUsers[user.id] = user;
         rtcUsers[iUserId].audio = false;
         io.sockets.emit('mute', iUserId)
     });
     socket.on('unmute', function(){
-        console.log('unmute', iUserId);
+        if (isDebug) {
+            console.log('unmute', iUserId);
+        }
+
         rtcUsers[user.id] = user;
         rtcUsers[iUserId].audio = true;
         io.sockets.emit('unmute', iUserId)
     });
     socket.on('pause video', function(){
-        console.log('pause video', iUserId);
+        if (isDebug) {
+            console.log('pause video', iUserId);
+        }
+
         rtcUsers[iUserId].video = false;
         io.sockets.emit('pause video', iUserId)
     });
     socket.on('resume video', function(){
-        console.log('resume video', iUserId);
+        if (isDebug) {
+            console.log('resume video', iUserId);
+        }
+
         rtcUsers[user.id] = user;
         rtcUsers[iUserId].audio = true;
         io.sockets.emit('resume video', iUserId)
     });
 
     socket.on('disconnect', function(){
-        console.log(rtcUsers,user);
+        if (isDebug) {
+            console.log(rtcUsers,user);
+        }
+
         delete rtcUsers[user.id];
-        console.log(rtcUsers,sUserAvatar);
+        if (isDebug) {
+            console.log(rtcUsers,sUserAvatar);
+        }
+
         io.sockets.emit('user leaved', rtcUsers);
-        console.log("user disconnected", data.iUserId)
+        if (isDebug) {
+            console.log("user disconnected", data.iUserId);
+        }
     }.bind(this));
-    console.log(rtcUsers, iUserId)
+
+    if (isDebug) {
+        console.log(rtcUsers, iUserId);
+    }
 });
 
 function sendToUser(id, event, data) {
@@ -129,7 +196,9 @@ function sendToGroup(group, event, data) {
 app.post('/notification', function(req, res){
     x += 1;
     let data = req.body;
-    console.log(data);
+    if (isDebug) {
+        console.log(data);
+    }
     data.noticeId = x;
     sendToUser(data.user_id, "notification", data);
     data.title="";
